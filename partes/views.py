2c0f52ad3ee22469dff3eb22ebf3bccd7d585d4f
YetYSoft <juandelhoyo@gmail.com>
2022-04-07 16:35:02 +0200

    Error en static

--- partes/views.py
+++ partes/views.py
@@ -1,22 +1,15 @@
 from django.shortcuts import render,redirect
+from django.shortcuts import get_list_or_404, get_object_or_404
+
 from django.http import HttpResponse
-
-
-
-
-
 
 from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-from django.shortcuts import get_list_or_404, get_object_or_404
-
 from django.urls import reverse_lazy
 from django.urls import reverse
-
-import json
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
