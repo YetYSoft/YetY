0306eb8c31892512e4e12b1bd48984373487dcf0
YetYSoft <juandelhoyo@gmail.com>
2022-04-21 14:29:02 +0200

    manejo rutas staticas

--- ot/settings.py
+++ ot/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        
     }
 }
 
@@ -121,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'),)
+STATIC_URL = 'static/'  
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static'),]
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL= '/'
