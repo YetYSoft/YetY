f2e7bc42ef7b2b4c9efe4c14b64b36cc58305cd6
YetYSoft <juandelhoyo@gmail.com>
2022-05-02 22:26:32 +0200

    Formulario piscina grande

--- piscinas/urls.py
+++ piscinas/urls.py
@@ -7,11 +7,12 @@ from piscinas import views
 
 
 urlpatterns = [
-    path ('piscinas/', views.piscinas_start, name='piscinas_start'),
-    path ('piscina_grande/', views.piscina_grande_registro, name='piscinas_start'),
+    path ('', views.piscinas_start, name='piscinas_start'),
+    path ('piscina_grande/', views.piscina_grande, name='piscina_grande_form'),
     path ('piscinas/piscina_pequeña/', views.piscinas_start, name='piscinas_start'),
     path ('piscinas/spa/', views.piscinas_start, name='piscinas_start'),
     path ('piscinas/jacuzzy/', views.piscinas_start, name='piscinas_start'),
 
 
 ]
+ 
--- piscinas/views.py
+++ piscinas/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from piscinas.forms import Piscina_grande_Form
+from piscinas.forms import Form_Piscina_grande
 from piscinas.models import Valores_Piscina
 
 
@@ -12,15 +12,16 @@ from piscinas.models import Valores_Piscina
 @login_required
 def piscinas_start(request):
        return render(request,"start_piscinas.html")
-       
+
+
 # Formulario de registro valores piscina grande
 @login_required
-def piscina_grande_registro(request):
+def piscina_grande(request):
     if request.method == "POST":
-        form = Piscina_grande_Form(request.POST)
+        form = Form_Piscina_grande(request.POST)
         if form.is_valid():
             form.save()
         return redirect('piscinas/piscinas')
     else:
-        form=Piscina_grande_Form()
-    return render (request,'grid_piscinas.html',{'form':form})
+       form = Form_Piscina_grande(request.POST)
+    return render (request,"piscina_grande_form.html", {"form": form}) 
