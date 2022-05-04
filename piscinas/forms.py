3c67f3159f63a7b71d9fb8e0687d9fed7dc09557
YetYSoft <juandelhoyo@gmail.com>
2022-05-04 16:48:14 +0200

    Piscina grande form

--- piscinas/forms.py
+++ piscinas/forms.py
@@ -29,5 +29,16 @@ class Form_Piscina_grande(forms.ModelForm):
         ]
         widgets = {
             'Ph': forms.NumberInput(attrs={'placeholder': 'Ph      (Entre 6,8 y 7,2)'}),
-            'cloro_libre': forms.NumberInput(attrs={'placeholder': 'Cloro      (Entre 1,0 y 1,5)'}),
-        }
+            'cloro_libre':  forms.NumberInput (attrs={'placeholder': 'Cloro      (Entre 1,5 y 2,0)'}),
+            'cloro_total':  forms.NumberInput (attrs={'placeholder': 'Libre (Max 0,6 mayor que cloro)'}),
+            'transparencia':forms.NumberInput (attrs={'placeholder': 'Transparencia   (Bien 1 si no 0)'}),
+            'turbidez':     forms.NumberInput (attrs={'placeholder': 'Turbidez   (Entre 0 y 5)'}),
+            'presion_filtro':forms.NumberInput (attrs={'placeholder': 'Presion filtro (Menor que 1)'}),
+            'presion_filtro_limpiado':forms.NumberInput (attrs={'placeholder': 'Filtro limpio    (Menor que 1)'}),
+            'nivel_cloro':forms.NumberInput (attrs={'placeholder': 'Nivel Cloro    (mayor del 75%)'}),
+            'nivel_ph':forms.NumberInput (attrs={'placeholder': 'Nivel Ph (mayor del 75%)'}),
+            'Incidencias':forms.TextInput(attrs={'placeholder': 'Que está pasando?'}),
+            'intervencion_realizada':forms.TextInput(attrs={'placeholder': 'Que se ha hecho?'}),
+            'rellenado_cloro':forms.CheckboxInput ( attrs={'placeholder': 'Marcar si se ha rellenado ahora'}),
+
+        }   
