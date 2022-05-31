6c8068a55f685f22d27ab3abded461aff666ce7a
YetYSoft <juandelhoyo@gmail.com>
2022-05-31 19:53:15 +0200

    Añadida bd

--- espacios/urls.py
+++ espacios/urls.py
@@ -16,8 +16,6 @@ urlpatterns = [
     path ('Plantas_o_zonas_list/', views.Plantas_o_zonas_list.as_view(), name='Plantas_o_zonas_list'), # Plantas_o_zonas_list
     path ('Plantas_o_zonas_edit/<int:pk>', views.Plantas_o_zonas_edit, name='Plantas_o_zonas_edit'), # Plantas_o_zonas_list
 
-
-
     path ('Hab_cuarto_sala_nueva/', views.Hab_cuarto_sala_nueva, name='Hab_cuarto_sala_nueva'), # Planta_o_zona_nueva
     path ('Hab_cuarto_sala_list/', views.Hab_cuarto_sala_list.as_view(), name='Hab_cuarto_sala_list'), # Plantas_o_zonas_list
     path ('Hab_cuarto_sala_edit/<int:pk>', views.Hab_cuarto_sala_edit , name='Hab_cuarto_sala_edit'), # Plantas_o_zonas_list
